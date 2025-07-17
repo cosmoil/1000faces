7a52a4700703fa57e979de0a70dc1d104080b5b5
cosmoil <einfachcosmoyt@gmail.com>
2025-07-17 14:03:37 +0200

    First version

--- app/src/main/java/com/cosmo/thousandfaces/view/ImageAdapter.java
+++ app/src/main/java/com/cosmo/thousandfaces/view/ImageAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import com.cosmo.thousandfaces.R;
 import com.cosmo.thousandfaces.model.ImageModel;
