8c0a53610a81945d7bc8851a5972581c491f11a3
cosmoil <einfachcosmoyt@gmail.com>
2025-07-19 00:32:35 +0200

    Logout button added

--- app/src/main/java/com/cosmo/thousandfaces/view/HomeActivity.java
+++ app/src/main/java/com/cosmo/thousandfaces/view/HomeActivity.java
@@ -1,6 +1,5 @@
 package com.cosmo.thousandfaces.view;
 
-import com.cosmo.thousandfaces.view.CameraActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,7 +9,7 @@ import com.cosmo.thousandfaces.R;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Button btnCamera, btnGallery, btnExport, btnSettings;
+    Button btnCamera, btnGallery, btnExport, btnLogOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +19,16 @@ public class HomeActivity extends AppCompatActivity {
         btnCamera = findViewById(R.id.btnCamera);
         btnGallery = findViewById(R.id.btnGallery);
         btnExport = findViewById(R.id.btnExport);
-        btnSettings = findViewById(R.id.btnSettings);
+        btnLogOut = findViewById(R.id.btnLogOut);
 
         btnCamera.setOnClickListener(v -> startActivity(new Intent(this, CameraActivity.class)));
         btnGallery.setOnClickListener(v -> startActivity(new Intent(this, GalleryActivity.class)));
         btnExport.setOnClickListener(v -> startActivity(new Intent(this, ExportActivity.class)));
-        btnSettings.setOnClickListener(v -> startActivity(new Intent(this, SettingsActivity.class)));
+        btnLogOut.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish(); // Close HomeActivity
+        });
     }
 }
--- app/src/main/java/com/cosmo/thousandfaces/view/SettingsActivity.java
+++
@@ -1,4 +0,0 @@
-package com.cosmo.thousandfaces.view;
-
-public class SettingsActivity {
-}
